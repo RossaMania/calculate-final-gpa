005eab727fbe872ad8cbd74cbb14eede2fc8a439
RossaMania <raclettenberg@gmail.com>
2024-05-04 18:55:53 -0500

    Initial commit. Add initial project and test files, and gitignore file.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace calculate_final_gpa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+
+}
