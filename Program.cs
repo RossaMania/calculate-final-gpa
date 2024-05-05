de45d5d02c9d79974978bca5c86b0ee9938e38ff
RossaMania <raclettenberg@gmail.com>
2024-05-04 20:29:29 -0500

    chore: write test file for nunit testing.

--- Program.cs
+++ Program.cs
@@ -64,6 +64,7 @@ namespace calculate_final_gpa
           Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
 
           Console.WriteLine($"\nFinal GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+
         }
 
     }
